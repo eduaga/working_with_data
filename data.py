9155b3ccd1ba0ea5a64f2eb3e3532689c3b1ecbc
eum <agadzhanoff@mail.ru>
2020-07-04 18:24:26 +0300

    Fix #1

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import json
+import xml.etree.ElementTree as ET
 
 
 def retrieve_news_json(json_file):
@@ -7,19 +8,32 @@ def retrieve_news_json(json_file):
         news_retrieve = news["rss"]["channel"]["items"]
     return news_retrieve
 
+def retrieve_news_from_xml(xml_file):
+    parser = ET.XMLParser(encoding="utf-8")
+    news_tree = ET.parse(xml_file, parser)
+    root = news_tree.getroot()
+    news_from_file = root.findall("channel/item/description")
+    return news_from_file
 
-def top_ten_from_retrieved_list(retr_list):
+def top_ten_from_retrieved_list(retr_list, format):
     top_dict = {}
     words_list = []
-    for news_blocks in retr_list:
-        words_list = news_blocks["description"].split()
+    for news in retr_list:
+        if format == "json":
+            words_list = news["description"].split()
+        elif format == "xml":
+            words_list = news.text.split()
         for word in words_list:
+            word = word.lower()
             if len(word) > 6:
                 if word in top_dict:
                     top_dict[word] += 1
                 else:
                     top_dict[word] = 1
-    print(f'Список наиболее популярных вхождений в тексте:\n{sorted(top_dict, key=top_dict.get, reverse=True)[0:10]}')
+    for frequency in sorted(top_dict, key=top_dict.get, reverse=True)[0:10]:
+        print(f'{frequency} - {top_dict[frequency]} вхождений')
 
 
-top_ten_from_retrieved_list(retrieve_news_json("newsafr.json"))
+
+top_ten_from_retrieved_list(retrieve_news_json("newsafr.json"), "json")
+#top_ten_from_retrieved_list(retrieve_news_from_xml("newsafr.xml"), "xml")
